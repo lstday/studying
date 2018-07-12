b38b41ab37333bcb7fbb31c47d01b4e0b8420ca1
lstday <sakhar.medovich@gmail.com>
2018-07-12 15:17:23 +0300

    restructuring, puma starting itself, mongo ip:port not added

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -5,3 +5,6 @@ git clone https://github.com/Artemmkin/reddit.git
 cd reddit
 source ~/.rvm/scripts/rvm
 bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl enable puma.service
+sudo systemctl start puma.service
