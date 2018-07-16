550e88f9b8e42e15c14a20b53cd47918b532f2fd
lstday <sakhar.medovich@gmail.com>
2018-07-16 14:22:01 +0300

    ansible install and deploy

--- tform/modules/vpc/variables.tf
+++ tform/modules/vpc/variables.tf
@@ -1,3 +1,7 @@
+#variable public_key_path {
+#  description = "Path to the public key used to connect to instance"
+#}
+
 variable source_ranges {
   description = "Allowed IP addresses"
   default     = ["0.0.0.0/0"]
